50932bf1ecd92aef43387394a809594c87188f56
河村 泰佑 <kata.kawa813@gmail.com>
2021-04-25 13:19:49 +0900

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,14 @@
 <script lang="ts">
 	import Tailwindcss from './Tailwindcss.svelte';
 	import Header from './components/Header.svelte';
-
-	export let name: string;
+	import Router from 'svelte-spa-router'
+  import { routes } from './router'
 </script>
 
 <Tailwindcss />
 <Header />
 <main class="pt-16">
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="container mx-auto px-6 my-4">
+    <Router {routes} />
+  </div>
 </main>
-
-<!-- <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style> -->
