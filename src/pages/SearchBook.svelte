8d98e2faa92a50dc55e45ad438a2e29f35629e1f
河村 泰佑 <kata.kawa813@gmail.com>
2021-04-30 20:44:23 +0900

    add

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,3 +1,19 @@
-<div>
-  本を探す
+<form>
+<form on:submit|preventDefault={handleSubmit}></form>
+  <SearchBar bind:value={q} />
+</form>
+
+<div class="text-center mt-4">
+  { q }
 </div>
+
+<script>
+ import SearchBar from "../components/SearchBar.svelte";
+
+ let q = 'JavaScript'
+
+const handleSubmit = () => {
+  console.log('handleSubmit')
+  console.log(q)
+}
+</script>
