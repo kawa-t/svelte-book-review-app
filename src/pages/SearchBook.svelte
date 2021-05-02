d4fb5197420be61b31ace9ca6a228d05206f7359
河村 泰佑 <kata.kawa813@gmail.com>
2021-05-02 21:12:08 +0900

    add

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,19 +1,46 @@
+<script>
+  import SearchBar from '../components/SearchBar.svelte'
+  import type { BookItem, Result } from '../repositories/book';
+  import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory'
+  const BookRepository = RepositoryFactory[BOOK]
+
+  let q = ''
+  let empty = false
+  let books: BookItem[] = []
+  let promise: Promise<void>
+
+  const handleSubmit = () => {
+    if (!q.trim()) return
+    promise = getBooks()
+  }
+
+  const getBooks = async () => {
+    books = []
+    empty = false
+    const result = await BookRepository.get({ q })
+    empty = result.totalItems === 0
+    books = result.items
+  }
+</script>
+
 <form>
 <form on:submit|preventDefault={handleSubmit}></form>
   <SearchBar bind:value={q} />
 </form>
 
 <div class="text-center mt-4">
-  { q }
+  {#if empty}
+    <div>検索結果が見つかりませんでした。</div>
+  {:else}
+    {#each books as book (book.id)}
+      <div>{book.volumeInfo.title}</div>
+    {/each}
+  {/if}
+  {#await promise}
+    <div>loading...</div>
+    {:catch e}
+      <span class="text-red-600 text-sm">
+        {e.message}
+      </span>
+  {/await}
 </div>
-
-<script>
- import SearchBar from "../components/SearchBar.svelte";
-
- let q = 'JavaScript'
-
-const handleSubmit = () => {
-  console.log('handleSubmit')
-  console.log(q)
-}
-</script>
