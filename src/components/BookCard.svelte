0c2cc46e34e3cf5a89c020e443833469a8251e2d
河村 泰佑 <kata.kawa813@gmail.com>
2021-05-03 18:30:39 +0900

    add

--- src/components/BookCard.svelte
+++ src/components/BookCard.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
- import type { BookItem } from "../repositories/book"
- export let book: BookItem
+  import type { BookItem } from '../repositories/book'
+  import { link } from 'svelte-spa-router'
+  export let book: BookItem
 
-
-   $: src = book.volumeInfo.imageLinks
+  $: src = book.volumeInfo.imageLinks
     ? book.volumeInfo.imageLinks.smallThumbnail
     : 'http://placehold.jp/eeeeee/cccccc/160x120.png?text=No%20Image'
 
@@ -18,7 +18,9 @@
   </div>
   <div class="border-r border-b border-l border-grey-light sm:border-l-0 sm:border-t sm:border-grey-light bg-white rounded-b sm:rounded-b-none sm:rounded-r p-4 flex flex-col justify-between leading-normal w-100 sm:w-9/12 lg:w-7/12">
     <div class="my-4">
-      <div class="text-black font-bold text-xl mb-2">{book.volumeInfo.title}</div>
+      <a href={`/books/${book.id}`} use:link>
+        <div class="text-black font-bold text-xl mb-2">{book.volumeInfo.title}</div>
+      </a>
       <p class="text-grey-darker text-sm break-words w-9/12 m-auto">
         {description}
       </p>
